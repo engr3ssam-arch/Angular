31519e7e474a2099aaedba1c255414dcff85d070
unknown <eng.r3ssam@gmail.com>
2026-02-19 16:37:51 +0200

    Modify Service from localstorage to on the fly

--- src/app/core/dataServices/data-service.ts
+++ src/app/core/dataServices/data-service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { User } from '../../shared/interface/user';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,36 +8,39 @@ import { User } from '../../shared/interface/user';
 export class DataService {
 
 private readonly STORAGE_KEY = 'registerUser';
+private usersSubject = new BehaviorSubject<any[]>(this.getStoredUsers());
+users$ = this.usersSubject.asObservable();
+
+   private getStoredUsers(): any[] {
+    const data = localStorage.getItem(this.STORAGE_KEY);
+    return data ? JSON.parse(data) : [];
+  }
+
+
 
  registerUser(userData: User) {
     
-    let users = JSON.parse(localStorage.getItem(this.STORAGE_KEY) || '[]');
-    
-    users.push(userData);
+    const currentUsers = this.usersSubject.value;
+    const updatedUsers = [...currentUsers, userData];
+    this.usersSubject.next(updatedUsers);
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(updatedUsers));
    
-    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(users));
-    
-    
-    alert('Data has been saved');
-    console.log(users);
-    
   }
   
 constructor() { }
 
   login(credentials: any): { success: boolean, user?:any } {
-     const users = JSON.parse(localStorage.getItem(this.STORAGE_KEY) || '[]');
+  const users = this.usersSubject.value;
     const foundUser = users.find((u: any) => 
       u.email === credentials.email && u.password === credentials.password
     );
 
     if (foundUser) {
-     localStorage.setItem('currentUser', JSON.stringify(foundUser));
+      localStorage.setItem('currentUser', JSON.stringify(foundUser));
       return { success: true, user: foundUser };
     }
 
     return { success: false };
   }
-
   
 }
