3b24fd695671c3bb6d1210d81849948792a63c82
unknown <eng.r3ssam@gmail.com>
2026-02-21 21:34:09 +0200

    Modify Nav Bar By adding user name to it upon login

--- src/app/shared/components/nav/nav.ts
+++ src/app/shared/components/nav/nav.ts
@@ -1,16 +1,25 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonEngine } from '@angular/ssr/node';
-
+import { DataService } from '../../../core/dataServices/data-service';
+import { Observable } from 'rxjs';
+import { User } from '../../interface/user';
 @Component({
   selector: 'app-nav',
   imports: [RouterLink ,RouterLinkActive ,CommonModule],
   templateUrl: './nav.html',
   styleUrl: './nav.scss',
 })
-export class Nav {
+export class Nav  {
+ 
+
+  currentUser$: Observable<User| null>;
+
+  constructor(private authService: DataService) {
   
+  this.currentUser$ = this.authService.currentUser$;
+  }
 
   isMenuCollapsed = true;
   toggleNavbar() {
