9039e91caeb61d2206e558dcf68b910b80250b24
unknown <eng.r3ssam@gmail.com>
2026-04-14 10:34:00 +0200

    Add Users Service

---
+++ src/app/Users/interfaces/users.ts
@@ -0,0 +1,24 @@
+export interface Users {
+  id ?: number;
+  firstName: string;
+  lastName: string;
+  maidenName :string;
+  age :number;
+  gender :string;
+  email: string;
+  phone:number;
+  userName:string;
+  birthDate:number;
+  height:number;
+  weight:number;
+  streetAdd:string;
+  city:string;
+  state:string;
+  zipCode:number;
+  coordinates:string;
+  university:string;
+  companyName:string;
+  department:string;
+  jobTitle:string;
+  
+}
